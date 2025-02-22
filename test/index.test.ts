58d0b6d03b175726e9519878d1b01f8a87ae25a9
jude <matisowagm@gmail.com>
2025-02-22 11:18:38 +0100

    Add tests for `compose`

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,5 +1,5 @@
 import { test, expect, vi, describe } from 'vitest'
-import { d } from '../src'
+import { d, diopter } from '../src'
 
 describe('identity', () => {
   test('not a prism', () => {
@@ -1005,17 +1005,102 @@ describe('pick', () => {
 })
 
 describe('compose', () => {
-  test('is a prism when left is a prism')
-  test('is a prism when right is a prism')
-  test('is a prism when left and right are prisms')
-  test('is not a prism when left and right are not prisms')
+  test('is a prism when left is a prism', () => {
+    const data = { a: { b: 1 } } as { a: { b: 1 } | undefined }
 
-  test('with another build-in diopter', () => {
+    const inner = d<{ b: 1 }>().b
+    const diopter = d<typeof data>().a.opt().compose(inner)
+
+    expect(diopter.isPrism).toBe(true)
+  })
+  test('is a prism when right is a prism', () => {
+    const data = { a: { b: 1 } } as { a: { b: 1 | undefined } }
+
+    const inner = d<{ b: 1 }>().b.opt()
+    const diopter = d<typeof data>().a.compose(inner)
+
+    expect(diopter.isPrism).toBe(true)
+  })
+  test('is a prism when left and right are prisms', () => {
+    const data = { a: { b: 1 } } as { a: { b: 1 | undefined } | undefined }
+
+    const inner = d<{ b: 1 }>().b.opt()
+    const diopter = d<typeof data>().a.opt().compose(inner)
+
+    expect(diopter.isPrism).toBe(true)
+  })
+  test('is not a prism when left and right are not prisms', () => {
+    const data = { a: { b: 1 } }
+
+    const inner = d<{ b: 1 }>().b
+    const diopter = d<typeof data>().a.compose(inner)
+
+    expect(diopter.isPrism).toBe(false)
+  })
+
+  test('with another build-in diopter > get', () => {
     const data = { a: { b: 1 } }
 
     const inner = d<(typeof data)['a']>().b
     const diopter = d<typeof data>().a.compose(inner)
 
     expect(diopter.get(data)).toBe(1)
+  })
+
+  test('with another build-in diopter > get', () => {
+    const data = { a: { b: 1 } }
+
+    const inner = d<(typeof data)['a']>().b
+    const diopter = d<typeof data>().a.compose(inner)
+
+    expect(diopter.set(data, () => 2)).toEqual({ a: { b: 2 } })
+  })
+
+  test('with another build-in diopter > mod', () => {
+    const data = { a: { b: 1 } }
+
+    const inner = d<(typeof data)['a']>().b
+    const diopter = d<typeof data>().a.compose(inner)
+
+    expect(diopter.set(data, (x) => x * 10)).toEqual({ a: { b: 10 } })
+  })
+
+  test('with custom diopter > get', () => {
+    const data = { a: { b: 1 } }
+
+    const custom = diopter<{ b: number }, number, false>({
+      get: (val) => val.b,
+      set: (val, modFn) => ({ ...val, b: modFn(val.b) }),
+    })
+
+    const myDiopter = d<typeof data>().a.compose(custom)
+
+    expect(myDiopter.get(data)).toBe(1)
+  })
+
+  test('with custom diopter > set', () => {
+    const data = { a: { b: 1 } }
+
+    const custom = diopter<{ b: number }, number, false>({
+      get: (val) => val.b,
+      set: (val, modFn) => ({ ...val, b: modFn(val.b) }),
+    })
+
+    const myDiopter = d<typeof data>().a.compose(custom)
+
+    expect(myDiopter.set(data, () => 2)).toEqual({ a: { b: 2 } })
+  })
+
+  test('with custom diopter > mod', () => {
+    const data = { a: { b: 1 } }
+
+    const custom = diopter<{ b: number }, number, false>({
+      get: (val) => val.b,
+      set: (val, modFn) => ({ ...val, b: modFn(val.b) }),
+    })
+
+    const myDiopter = d<typeof data>().a.compose(custom)
+
+    expect(myDiopter.set(data, (x) => x * 10)).toEqual({ a: { b: 10 } })
   })
 })
