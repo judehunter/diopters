cff65c39de3cb8ed7d87ce8bfbbf1b1acf996d54
jude <matisowagm@gmail.com>
2025-02-22 11:18:22 +0100

    🔧 Prototype Part 1

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,111 @@
 export {}
 
-type Lens<A, B> = {
+type Dioptre<A, B> = {
   get(a: A): B
-  _<C>(
-    bc: Lens<B, C>,
-  ): Lens<A, HasUndefined<B> extends true ? C | undefined : C>
+  set(a: A, mod: (b: Values<B>) => Values<B>): A
+} & Std<A, B> & {
+    [K in keyof B | KeyOfDistr<B>]: K extends keyof B
+      ? undefined extends B[K]
+        ? Dioptre<A, NonNullable<B[K]> | undefined>
+        : Dioptre<A, B[K]>
+      : K extends KeyOfDistr<B>
+        ? Dioptre<A, AccessDistr<B, K> | undefined>
+        : never
+  }
+
+type Std<A, B> = {
+  compose<C>(
+    bc: Dioptre<B, C>,
+  ): Dioptre<A, HasUndefined<B> extends true ? C | undefined : C>
+
+  // path<K extends keyof B | KeyOfDistr<B>>(
+  //   key: K,
+  // ): K extends keyof B
+  //   ? undefined extends B[K]
+  //     ? Dioptre<A, NonNullable<B[K]> | undefined>
+  //     : Dioptre<A, B[K]>
+  //   : K extends KeyOfDistr<B>
+  //     ? Dioptre<A, AccessDistr<B, K> | undefined>
+  //     : never
+
+  map<C>(
+    mapFn: (valueOfA: Dioptre<Values<B>, Values<B>>) => Dioptre<Values<B>, C>,
+  ): Dioptre<A, NonNullable<C>[]>
+
+  find<C extends B>(predicate: (b: B) => b is C): Dioptre<A[], C[]>
 }
 
-const lens = <A, B>({ get }: { get: (a: A) => B }): Lens<A, B> => {
+const std = <A, B>(ab: Dioptre<A, B>) => {
+  const compose: Std<A, B>['compose'] = (bc) => composeLens(ab, bc)
+
+  const path: Std<A, B>['path'] = (key) =>
+    dioptre({
+      get: (a: {}) => {
+        if (typeof a === 'object' && key in a) {
+          return a[key as any]
+        }
+        return undefined
+      },
+    }) as any
+
+  const map: Std<A, B>['map'] = (mapFn) =>
+    dioptre({
+      get: ((a: A) => {
+        if (!Array.isArray(a)) {
+          throw new Error('Not an array')
+        }
+        return (
+          a
+            .map((elem, i) => {
+              const elemLens = dioptre({
+                get: (_a: A) => (_a as unknown as any[])[i] as Values<A>,
+              })
+              const transformedLens = mapFn(elemLens as any)
+              const transformed = transformedLens.get(a as any)
+
+              return transformed
+            })
+            // todo: maybe it's fine to return undefs/nulls from traversals?
+            .filter(
+              (x): x is NonNullable<typeof x> => x !== undefined && x !== null,
+            )
+        )
+      }) as any,
+    })
+
+  const find = (predicate: (b) => boolean) =>
+    map((x) =>
+      x.compose(
+        dioptre({
+          get: (a) => {
+            if (predicate(a)) {
+              return a
+            }
+            return null!
+          },
+        }),
+      ),
+    )
+
+  return { compose, path, map }
+}
+
+const dioptre = <A, B>({ get }: { get: (a: A) => B }): Dioptre<A, B> => {
   const thisLens = {
     get,
-    _: <C>(bc: Lens<B, C>) => compose(thisLens, bc),
-  } satisfies Lens<A, B>
-  return thisLens
+  }
+  Object.assign(thisLens, std(thisLens as any))
+
+  return thisLens as any
 }
 
 type HasUndefined<T> = undefined extends T ? true : false
 
-const compose = <A, B, C>(
-  ab: Lens<A, B>,
-  bc: Lens<NonNullable<B>, C>,
-): Lens<A, HasUndefined<B> extends true ? C | undefined : C> => {
-  return lens({
+const composeLens = <A, B, C>(
+  ab: Dioptre<A, B>,
+  bc: Dioptre<NonNullable<B>, C>,
+): Dioptre<A, HasUndefined<B> extends true ? C | undefined : C> => {
+  return dioptre({
     get: ((a) => {
       const b = ab.get(a)
       if (b === undefined) {
@@ -32,8 +116,8 @@ const compose = <A, B, C>(
   })
 }
 
-const _ = <A>(): Lens<A, A> =>
-  lens({
+const id = <A>(): Dioptre<A, A> =>
+  dioptre({
     get: (a: A) => a,
   })
 
@@ -47,72 +131,7 @@ type Values<T> = T extends any[]
     ? T[keyof T]
     : never
 
-// maybe can be simplified to just use KeyOfDistr only?
-const path = <T, K extends keyof T | KeyOfDistr<T>>(
-  key: K,
-): K extends keyof T
-  ? undefined extends T[K]
-    ? Lens<T, NonNullable<T[K]> | undefined>
-    : Lens<T, T[K]>
-  : K extends KeyOfDistr<T>
-    ? Lens<T, AccessDistr<T, K> | undefined>
-    : never =>
-  ({
-    get: (obj: T) => null!,
-  }) as any
-
-// const required = <T>(): Lens<T, NonNullable<T>> => ({
-//   get: (a) => {
-//     if (a === undefined || a === null) {
-//       throw new Error('Undefined')
-//     }
-//     return a
-//   },
-//   type: 'lens',
-// })
-
-const map = <A, B>(
-  mapFn: (valuesOfA: Lens<A, Values<A>>['_']) => Lens<A, B>,
-): Lens<A, NonNullable<B>[]> =>
-  lens({
-    get: ((a: A) => {
-      if (!Array.isArray(a)) {
-        throw new Error('Not an array')
-      }
-      return (
-        a
-          .map((elem, i) => {
-            const elemLens: Lens<A, Values<A>> = lens({
-              get: (_a: A) => (_a as unknown as any[])[i] as Values<A>,
-            })
-            const transformedLens = mapFn(elemLens._)
-            const transformed = transformedLens.get(a)
-
-            return transformed
-          })
-          // todo: maybe it's fine to return undefs/nulls from traversals?
-          .filter(
-            (x): x is NonNullable<typeof x> => x !== undefined && x !== null,
-          )
-      )
-    }) as any,
-  })
-
-const find = <A, B extends A>(predicate: (a: A) => a is B): Lens<A[], B[]> =>
-  map((_) =>
-    _(
-      lens({
-        get: (a) => {
-          if (predicate(a)) {
-            return a
-          }
-          return null!
-        },
-      }),
-    ),
-  )
-
-const testLens = _<{ addresses: { street?: { name: string } }[] }>()
+const testLens = id<{ addresses: { street?: { name: string } }[] }>()
   ._(path('addresses'))
   ._(map((_) => _(path('street'))._(path('name'))))
 
@@ -123,4 +142,44 @@ type Person = {
 type Street = { num: number; name: string }
 type Address = { city: string; street: Street }
 
-const test = _<Person>()._(path('test'))._(path('deep'))._(path('stuff'))
+const test = id<Person>()._(path('test'))._(path('deep'))._(path('stuff'))
+
+const d = id
+
+d<Person>()
+  .path('addresses')
+  .map((x) => x.path('street'))
+  .get()
+
+d<{ a: { b: { c: 123 }[] }[] }>()
+  .path('a')
+  .map((x) => x.path('b').map((x) => x.path('c')))
+  .get()
+
+
+
+  const data = null!
+
+
+
+
+  
+
+const value = d<{ a: {b?: {c: 123}} }>().a.b.c.get(data)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+console.log(value)
